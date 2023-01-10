01da9bf916bfaf55b89cbbae3cfae8570f4a7181
student <student@PC-G-04>
2023-01-10 20:41:53 +0200

    5

--- main.cpp
+++ main.cpp
@@ -2,18 +2,19 @@
 
 int main()
 {
-    int grn;
-    std::cout << "grn->";
-    std::cin >> grn;
-    int cop;
-    std::cout << "cop->";
-    std::cin >> cop;
-    double a;
-    a = cop % 100;
-    double b;
-    b = (cop - a) / 100;
-    grn = grn + b;
-    std::cout << grn << "грн " << a << "коп";
+	float r;
+	std::cout << "r->";
+	std::cin >> r;
+	r = r * r * r;
+	float n;
+	n = 1.33;
+	float p;
+	p = 3.14;
 
-    return 0;
+	float volume = n * p * r;
+
+	std::cout << "Ball volume:" << volume << " cm3";
+
+	return 0;
+
 }
